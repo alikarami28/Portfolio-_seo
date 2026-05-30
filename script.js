379cb26732529bcf7d59276ef7aa97937b4638a6
alikarami28 <alikarami28@gmail.com>
2026-05-30 22:24:54 +0330

    دکمه اسکرول

--- script.js
+++ script.js
@@ -528,6 +528,7 @@ document.addEventListener('DOMContentLoaded', function() {
     /* =====================================================================
        ۱۲. دکمه برگشت به بالا
        ===================================================================== */
+
     var scrollBtn = document.createElement('button');
     scrollBtn.className = 'scroll-to-top';
     scrollBtn.innerHTML = '<i class="fa-solid fa-arrow-up"></i>';
